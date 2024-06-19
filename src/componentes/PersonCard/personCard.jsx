d7ff4ad101718dc5e11d3f23899d9621c2229c05
fceciliaaa <ceciferreira52@gmail.com>
2024-06-19 18:00:17 -0400

    Añadiendo componente

--- src/componentes/PersonCard/personCard.jsx
+++ src/componentes/PersonCard/personCard.jsx
@@ -2,18 +2,25 @@ import { Component } from "react";
 
 class PersonCard extends Component {
     constructor(props) {
-        super(props);
+        super(props); 
+        this.state = {
+            age: this.props.age
+        }
+    }
+    aumentarEdad = () => {
+        this.setState({ age: this.state.age + 1})
     }
 
     render = () => {
         return (
             <>
                 <h1> {this.props.lastName}, {this.props.firstName} </h1>
-                <h3>Edad: {this.props.age} </h3>
-                <h3>Color de cabello: {this.props.hairColor} </h3>
+                <h3> Edad: {this.state.age} </h3>
+                <h3> Color de cabello: {this.props.hairColor} </h3>
+                <button onClick={this.aumentarEdad}>Birthday button of {this.props.firstName} </button>
             </>
         );
     }
 }
 
-export default PersonCard;
+export default PersonCard
